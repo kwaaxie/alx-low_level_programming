f1b2e5e50049013a76725d126697d83d463eea34
kwaaxie <outta.att.net>
2022-10-17 05:32:33 +0000

    Task 4

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point 
+ * Description : Alphabet uppercase
+ * Return: 0 (successfull)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+	putchar(i);
+	}
+	for (i = 65; i <= 90; i++)
+	{
+		putchar(i);
+	} putchar ('\n');
+	return (0);
+}
