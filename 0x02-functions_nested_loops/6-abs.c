12691f8a5ef959d6f668ed80cf20ce833f96be3b
kwaaxie <outta.att.net>
2022-10-20 03:38:43 +0000

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * abs - cmputes the abs value  of an int
+ * @n: the int in the question
+ *
+ * Return: the abs of the int.
+ */
+int_abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
