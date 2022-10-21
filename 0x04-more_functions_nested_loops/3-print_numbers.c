be3d887ef82dca7907cd90e36bb0eea6ce9ec2b3
kwaaxie <outta.att.net>
2022-10-21 17:59:49 +0000

    Task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * print_numbers - prints number
+ * print_numbers - prints the numbers, from 0 to 9
+ * followed by a new line
  */
 
 void print_numbers(void)
@@ -10,7 +11,7 @@ void print_numbers(void)
 
 	for (i = 48; i < 58; i++)
 	{
-		_putchar(1);
+		_putchar(i);
 	}
 	_putchar('\n');
 }
