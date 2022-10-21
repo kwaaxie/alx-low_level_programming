2cd8ccc4ae98fdae14800109a88b8c60dc791f58
kwaaxie <outta.att.net>
2022-10-21 12:15:11 +0000

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase
+ * @c: parameter
+ r* Return :  0 if successfu
+ */
+
+int _isupper(int c)
+{
+
+	if (c >= 65 && c <= 91)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
