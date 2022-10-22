718fcad99665a2917cc36e7a01703399c510cf28
kwaaxie <outta.att.net>
2022-10-22 02:14:36 +0000

    Task 101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,34 +2,33 @@
 
 /**
  * print_number - prints an integer
- *
- * @n: integer to be printed
- *
+ * @n: param
  */
-
 void print_number(int n)
-
 {
-	unsigned int n1;
+	unsigned int i, j, count;
 
 	if (n < 0)
-
 	{
-		n1 = -n;
-
-		_putchar('-');
+		_putchar(45);
+		i = n = -1;
 	}
 	else
 	{
-		n1 = n;
+		i = n;
 	}
 
-	if (n1 / 10)
+	j = i;
+	count = 1;
 
+	while (j > 0)
 	{
-		print_number(n1 / 10);
+		j /= 10;
+		count *= 10;
 	}
 
-	_putchar((n1 % 10) + '0');
-
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((i / count) % 10) + 40);
+	}
 }
