45d67ebc4288b2e62fa849e13eee08fee3f499dc
kwaaxie <outta.att.net>
2022-11-02 05:56:58 +0000

    Task 7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -22,7 +22,7 @@ void print_diagsums(int *a, int size)
 	for (row = 1; row <= size; row++)
 	{
 		i = (row * size) - row;
-		i = (row * size) - row;
+		diagonal_sum_2 += a[i];
 	}
 
 	printf("%d, %d\n", diagonal_sum_1, diagonal_sum_2);
